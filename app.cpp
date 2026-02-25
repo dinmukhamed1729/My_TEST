deb3791898eeb8b07ce18c9b360ab9f9b0e523e6
dinmukhamed1729 <102159492+dinmukhamed1729@users.noreply.github.com>
2026-02-26 00:40:09 +0500

    second

--- app.cpp
+++ app.cpp
@@ -10,3 +10,8 @@
 using namespace std;
 
 
+struct Task {
+    int   type;          // 1 – шарт, 2 – for циклі, 3 – while циклі
+    string question;     // тапсырма мәтіні
+    int   correct;       // дұрыс жауап
+};
